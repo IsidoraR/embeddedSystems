01224eb586009520fe75958ec675d67fecea8b09
Isidora Radovanovic <ixr36@case.edu>
2017-10-11 00:21:50 +0000

    fixed bugs

--- dht11_interupt/dht11_interr.c
+++ dht11_interupt/dht11_interr.c
@@ -291,7 +291,7 @@ int generateChecksum(uint8_t* bits_rcvd)
 	for(int sum_idx= 0; sum_idx < 40; ++sum_idx)
 	{
 		int byte_num = (sum_idx/8);
-		int bit_num = 8-(sum_idx % 8);
+		int bit_num = 7-(sum_idx % 8);
 
 //		printf("Bit %d value: %d \n\r",sum_idx, bits_rcvd[sum_idx]);
 		printf("bit %d tume: %d \n\r", sum_idx, measuredBitHighTime[sum_idx]);
